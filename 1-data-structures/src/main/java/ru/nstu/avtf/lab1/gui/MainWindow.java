242edeb357c5d7aca6448304d380a4313fed9e76
Nikita Putko <nikita.putko@eltex.loc>
2020-12-28 13:45:11 +0700

    Make FXML load

--- 1-data-structures/src/main/java/ru/nstu/avtf/lab1/gui/MainWindow.java
+++ 1-data-structures/src/main/java/ru/nstu/avtf/lab1/gui/MainWindow.java
@@ -1,4 +1,56 @@
 package ru.nstu.avtf.lab1.gui;
 
+import javafx.event.ActionEvent;
+import javafx.scene.control.Button;
+import javafx.scene.control.ListView;
+import javafx.scene.control.MenuItem;
+import javafx.stage.Stage;
+import ru.nstu.avtf.lab1.gui.alerts.InformationAlert;
+
 public class MainWindow {
+    public MenuItem newListMenuItem = new MenuItem();
+    public MenuItem openListMenuItem = new MenuItem();
+    public MenuItem saveListMenuItem = new MenuItem();
+    public MenuItem closeMenuItem = new MenuItem();
+    public MenuItem aboutMenuItem = new MenuItem();
+
+    public Button addToEndButton = new Button();
+    public Button addMenuItem = new Button();
+    public Button removeMenuItem = new Button();
+    public Button sortMenuItem = new Button();
+
+    public ListView<Integer> listView = new ListView<>();
+
+    public void pressNewList() {
+    }
+
+    public void pressOpenList() {
+    }
+
+    public void pressSaveList() {
+    }
+
+    public void pressClose() {
+        ((Stage) listView.getScene().getWindow()).close();
+    }
+
+    public void pressAbout() {
+        new InformationAlert(
+                "О программе",
+                "Лабораторная №2. Связный список с графическим интерфейсом",
+                "Н.В. Путько, АММ2-20"
+                ).showAndWait();
+    }
+
+    public void pressAddToEnd() {
+    }
+
+    public void pressAdd() {
+    }
+
+    public void pressRemove() {
+    }
+
+    public void pressSort() {
+    }
 }
